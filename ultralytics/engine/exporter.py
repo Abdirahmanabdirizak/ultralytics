52d14dff7807cccf324008ff5a10a25c3065ba63
Lakshantha Dissanayake <lakshantha@ultralytics.com>
2026-04-21 16:55:35 -0700

    Set `TASK2CALIBRATIONDATA ` for deepx

--- ultralytics/engine/exporter.py
+++ ultralytics/engine/exporter.py
@@ -332,6 +332,8 @@ class Exporter:
             if not self.args.int8:
                 LOGGER.warning("DeepX export requires int8=True, setting int8=True.")
                 self.args.int8 = True
+            if not self.args.data:
+                self.args.data = TASK2CALIBRATIONDATA.get(model.task)
 
         if fmt == "axelera":
             if model.task == "segment" and any(isinstance(m, Segment26) for m in model.modules()):
